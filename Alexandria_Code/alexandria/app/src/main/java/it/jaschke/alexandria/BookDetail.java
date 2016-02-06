537b664997b8b45f2913cf444bdebe5c711d33d2
ashitakalax <leviballing@gmail.com>
2016-02-06 13:58:19 -0700

    found issue with transitioning from portrait to landscape. the crash was due to the shareActionProvider being null after the transition.

--- Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -96,11 +96,12 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
 
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text)+bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
-
+        if(shareActionProvider != null) {
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+            shareActionProvider.setShareIntent(shareIntent);
+        }
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
 
